b6c788880bcc4bc764402b62e57c27fb51ca0e9c
luiz.marin <luiz.marin@softplan.com>
2019-11-18 17:28:32 -0300

    First commit playerVideo

---
+++ index.js
@@ -0,0 +1,4 @@
+const cover = document.querySelector(".card-image");
+const title = document.querySelector(".card-content h5");
+const artist = document.querySelector(".artist");
+const audio = document.querySelector("audio");
